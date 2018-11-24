5e34d0723c978ef93d5c604f5d3090c0ccb7a08a
Rayce Rossum <raycer>
2018-11-24 14:52:09 -0800

    Update run_all.sh to render .Rmd

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,4 @@
 python src/1_pokemonData.py "./data/Pokemon.csv" "./data/Pokemon_clean.csv"
 Rscript src/2_pokemonEDA.R ./data/Pokemon_clean.csv ./results
 python src/3_pokemonAnalysis.py "./data/Pokemon_clean.csv" "./results/DecisionTree_important_features"
+Rscript -e "rmarkdown::render('./doc/Final_Report.Rmd')"
