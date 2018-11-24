9a0933facd86df50b3e4877569c676882f36c91a
Jack Yang <jackjiayan@hotmail.com>
2018-11-23 21:40:26 -0800

    nov 23

--- src/pokemonAnalysis.py
+++ src/pokemonAnalysis.py
@@ -8,7 +8,7 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
 #import pokemonData
 pokemon_data = pd.read_csv("./data/Pokemon_clean.csv")
 pokemon_data = pd.get_dummies(pokemon_data, columns=["Type 1", "Type 2"])
-
+pokemon_data.head()
 def main():
     #pokemon_data = pokemonData.get_pokemon_data(True)
     features = pokemon_data.loc[:, "Total":"Type 2_Water"]
@@ -19,9 +19,10 @@ def main():
     tree.fit(features, legendary)
     feature_importances = tree.feature_importances_
     importance_table = pd.DataFrame({"Feature": features.columns.values, "Importance": feature_importances})
+    importance_table
     importance_table = importance_table.query("Importance > 0.01")
     importance_table = importance_table.sort_values(by="Importance", ascending=False)
-
+    importance_table
     plt.rcParams["figure.figsize"] = (15, 4)
     plt.rcParams["xtick.labelsize"] = 10
     importance_plot = sns.barplot(data=importance_table, x="Feature", y="Importance")
