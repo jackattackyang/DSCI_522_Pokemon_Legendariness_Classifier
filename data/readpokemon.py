fa2a1b7f2fa298024e1c5ff1bc7fc7c5d09797eb
Rayce Rossum <raycer>
2018-11-24 13:08:46 -0800

    Remove random files

--- data/readpokemon.py
+++
@@ -1,8 +0,0 @@
-import sys
-import numpy as np
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn.feature_extraction.text import CountVectorizer
-
-pokemon = pd.read_csv("./data/Pokemon.csv")
-pokemon.head()
