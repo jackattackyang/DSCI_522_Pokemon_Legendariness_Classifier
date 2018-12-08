4dec3f18ededc38d09dabc2653b3396a3600394b
Jack Yang <jackjiayan@hotmail.com>
2018-12-08 12:14:05 -0800

    added main functions for clarity

--- src/4_predictorsPlot.R
+++ src/4_predictorsPlot.R
@@ -12,29 +12,32 @@ pokemon_table <- read_csv(input_file)
 
 #head(input_file)
 
-stat_color_template <- c(
-  "#F08030", # Attack
-  "#6890F0", # Sp. Attack
-  "#78C850", # Sp. Defense
-  "#F85888", # Speed
-  "#F8D030", # Defense
-  "#B8A038", # Rock
-  "#FF0000"  # HP
-)
-pokemon_table <- 
-  pokemon_table %>% 
-  mutate(Feature = factor(Feature, levels = c("Attack","Sp. Atk","Sp. Def", "Speed", "Defense", "Type 1_Rock", "HP")))
+main <- function(){
+    
+  stat_color_template <- c(
+    "#F08030", # Attack
+    "#6890F0", # Sp. Attack
+    "#78C850", # Sp. Defense
+    "#F85888", # Speed
+    "#F8D030", # Defense
+    "#B8A038", # Rock
+    "#FF0000"  # HP
+  )
+  pokemon_table <- 
+    pokemon_table %>% 
+    mutate(Feature = factor(Feature, levels = c("Attack","Sp. Atk","Sp. Def", "Speed", "Defense", "Type 1_Rock", "HP")))
+  
+  pokemon_table 
+  
+  (predictors_plot <- ggplot(pokemon_table, aes(fct_reorder(Feature, Importance, .desc=TRUE), Importance, 
+                         fill = Feature)) +
+    geom_bar(stat = "identity") + scale_fill_manual(values = stat_color_template) +
+    labs(x = "", title = "Top Predictors for Legendaries",
+         y = "Predictor %") +
+    theme(legend.position = "none"))
+  
+  ggsave("DecisionTree_important_features_plot.png", predictors_plot, path = output_file, width = 5, height =5)
 
-pokemon_table 
+}
 
-(predictors_plot <- ggplot(pokemon_table, aes(fct_reorder(Feature, Importance, .desc=TRUE), Importance, 
-                       fill = Feature)) +
-  geom_bar(stat = "identity") + scale_fill_manual(values = stat_color_template) +
-  labs(x = "", title = "Top Predictors for Legendaries",
-       y = "Predictor %") +
-  theme(legend.position = "none"))
-
-ggsave("DecisionTree_important_features_plot.png", predictors_plot, path = output_file, width = 5, height =5)
-
-# does not generate Rplot
-
+main()
